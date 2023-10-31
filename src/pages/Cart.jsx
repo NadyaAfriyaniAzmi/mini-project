fafa4e730846e98fba5fcff99454194237fa3717
Nadya afriyani azmi <nadyaafriyani45@gmail.com>
2023-11-01 01:29:00 +0800

    Update fitur cart

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -99,7 +99,7 @@ function Cart() {
           Cart
         </h2>
         <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-          <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+          <table className="w-full mb-16 text-sm text-left text-gray-500 dark:text-gray-400">
             <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
               <tr>
                 <th scope="col" className="px-6 py-3">
